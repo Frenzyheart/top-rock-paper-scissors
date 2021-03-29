8a297dcd0b1e779498fa21cabf8e053c238bf69f
Frenzyheart <frenzyheart.plays@gmail.com>
2021-03-29 01:40:24 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let computerPlay = () => {
+  const options = ["Rock", "Paper", "Scissors"];
+  let chosen = Math.floor(Math.random() * 3);
+  return options[chosen];
+}
+
+let playRound = (playerSelection, computerSelection) => {
+
+}
